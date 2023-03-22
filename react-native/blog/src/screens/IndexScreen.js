ad5984380b4ad76a5a08f5dee8f39203bfd5540a
bhavik <bpatel5493@gmail.com>
2023-03-22 18:23:17 -0500

    wip

--- react-native/blog/src/screens/IndexScreen.js
+++ react-native/blog/src/screens/IndexScreen.js
@@ -1,23 +1,20 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-import BlogContext from "../context/BlogContext";
+import { Context as BlogContext } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
-  // console.log(data.author);
-  console.log(data);
-  console.log(addBlogPost);
+  const { state, addBlogPost } = useContext(BlogContext);
 
-  // let author = data.author;
-  let posts = data;
+  console.log(state);
+  console.log(addBlogPost);
 
   return (
     <View>
       <Button onPress={addBlogPost} title="ADD BLOG" />
       {/* <Text>Author: {author.name}</Text> */}
       <FlatList
-        data={posts}
+        data={state}
         keyExtractor={(post) => post.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
