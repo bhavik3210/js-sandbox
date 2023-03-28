c5674fd3fa69d67b5ba82f9118d1ac3ff4932b1b
bhavik <bpatel5493@gmail.com>
2023-03-28 10:24:26 -0500

    wip

--- javascript/src/Index.ts
+++ javascript/src/Index.ts
@@ -1,7 +1,7 @@
 import demoVariables from "./Variables";
 import demoStrings from "./Strings";
 import demoFunctions from "./Functions";
-import demoPromises from "./promises";
+import demoPromises from "./Promises";
 import demoSpreadOperator from "./Spread";
 import demoBasicErrorHandlingWithTryCatch from "./ErrorHandling";
 
