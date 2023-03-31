a895bcf232e8b0088dfabd2e868119e260fac9cf
bhavik <bpatel5493@gmail.com>
2023-03-30 20:05:44 -0500

    wip

---
+++ dojo/building-applications-with-react-17-and-redux/src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p> This app uses React, redux, react router</p>
+  </div>
+);
+
+export default AboutPage;
