b04756f23324f642a08b11a3f1c0898870770152
bhavik <bpatel5493@gmail.com>
2023-04-08 06:02:15 -0500

    wip

--- dojo/typescript-4-projects/src/index.ts
+++ dojo/typescript-4-projects/src/index.ts
@@ -1,7 +1,11 @@
 // this is a comment
-import { Main } from "./app/Main";
+import { Main } from './app/Main'
+import { emphasizeString } from './typed/language'
 
-var message = "Hello Typscript";
-console.log(message);
+var message = 'Hello Typscript'
+console.log(message)
 
-new Main().render();
+new Main().render()
+
+var emphasizedMessage = emphasizeString(message, 3)
+console.log(emphasizedMessage)
