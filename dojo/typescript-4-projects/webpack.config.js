d4151dc5d7f1fa9809e7e4b10c3676eee3bcfded
bhavik <bpatel5493@gmail.com>
2023-04-08 06:26:54 -0500

    wip

--- dojo/typescript-4-projects/webpack.config.js
+++ dojo/typescript-4-projects/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'bin'),
   },
+  devtool: 'eval-source-map',
   devServer: {
     static: [path.join(__dirname, 'public'), path.join(__dirname, 'bin')],
     port: 7777,
