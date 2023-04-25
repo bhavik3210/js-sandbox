05c1dc8cad4bc60667d3618a46c90389d1a3f259
bhavik <bpatel5493@gmail.com>
2023-04-25 08:02:02 -0500

    new project

---
+++ javascript/src/lang/Objects.ts
@@ -0,0 +1,23 @@
+// #### Create object using object literal
+const objLiteral = {
+  balance: 500,
+};
+
+// #### Create object using a class
+class ClassObject {
+  balance: number = 1000;
+}
+const classObj = new ClassObject();
+
+// #### Create object using a function
+function FunctionObject() {
+  const balance = 9000;
+  function showMeBalance() {
+    console.log(balance);
+  }
+}
+const functionObj = new FunctionObject();
+functionObj.showMeBalance();
+
+// #### Create object using Object.create()
+const objCreate = Object.create(objLiteral);
