24c656b2c8a5396eca2d66b766dcfb54902924c7
bhavik <bhavik@work>
2023-06-28 06:57:36 -0500

    wip

--- fleeting/react/ps-react-18-fundamentals/components/banner.js
+++ fleeting/react/ps-react-18-fundamentals/components/banner.js
@@ -1,10 +1,24 @@
+import styles from "./banner.module.css";
+
 const Banner = () => {
+  const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+  };
+
   return (
-    <header>
-      <div>
-        <img src="./GloboLogo.png" alt="logo" />
+    <header className="row mb-4">
+      <div className="col-5">
+        <img className={styles.logo} src="./GloboLogo.png" alt="logo" />
       </div>
-      <div>Providing all over the world</div>
+      <div
+        className="col-7 mt-5"
+        // style={{ fontStyle: "italic", fontWeight: "bold" }}
+        style={subtitleStyle}
+      >
+        Providing houses all over the world
+      </div>
     </header>
   );
 };
